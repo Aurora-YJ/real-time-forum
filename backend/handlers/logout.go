8ddde234258d78efdabfe61123e51c910c2aadcd
Yossf-jaouhar <jwhryosf@gmail.com>
2025-03-19 14:08:44 +0000

    (:

---
+++ backend/handlers/logout.go
@@ -0,0 +1,46 @@
+package handlers
+
+import (
+	"database/sql"
+	"forum/backend/errors"
+	"net/http"
+)
+
+// Function to delete the session from the database
+func deleteSession(sessionID string, db *sql.DB) error {
+	_, err := db.Exec("DELETE FROM sessions WHERE session_id = ?", sessionID)
+	return err
+}
+
+// Logout handler function
+func LogoutHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	// Get the session ID from the cookie (assuming it's stored as "session_id")
+	sessionCookie, err := r.Cookie("session_id")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			errors.SendError( "No session found", http.StatusUnauthorized,w)
+		} else {
+			errors.SendError( "Error reading cookie", http.StatusBadRequest,w)
+		}
+		return
+	}
+
+	// Delete the session from the database
+	err = deleteSession(sessionCookie.Value, db)
+	if err != nil {
+		errors.SendError( "Error logging out", http.StatusInternalServerError,w)
+		return
+	}
+
+	// Optionally, clear the session cookie in the response
+	http.SetCookie(w, &http.Cookie{
+		Name:   "session_id",
+		Value:  "",
+		Path:   "/",
+		MaxAge: -1, // Expire the cookie immediately
+	})
+
+	// Respond with a success message
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Successfully logged out"))
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module forum
+
+go 1.23.0
+
+toolchain go1.23.6
+
+require (
+	github.com/gorilla/websocket v1.5.3
+	github.com/mattn/go-sqlite3 v1.14.24
+	golang.org/x/crypto v0.35.0
+)
+
+require (
+	github.com/gofrs/uuid/v5 v5.3.1 // indirect
+)
